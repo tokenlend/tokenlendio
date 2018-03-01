baad729bceb48f2670ce27dc63a7cde671cdbf73
stealth <stealth@foxportal.com>
2018-03-01 12:21:08 +0200

    add live network to truffle.js

--- truffle.js
+++ truffle.js
@@ -5,18 +5,35 @@ const providerRopsten = new HDWalletProvider(mnemonic, 'https://ropsten.infura.i
 const providerKovan = new HDWalletProvider(mnemonic, 'https://kovan.infura.io');
 
 module.exports = {
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  mocha: {
+    grep: '.'
+  },
   networks: {
+    live: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 1,
+      gas: 4000000,
+      gasPrice: 5000000000
+    },
     development: {
       host: 'localhost',
       port: 8545,
+      gasPrice: 2000000000,
+      gas: 4000000,
       network_id: '*', // Match any network id
     },
     testrpc: {
       host: 'localhost',
       port: 8545,
       network_id: '*', // Match any network id
-      gas: 4000000,
-      gasPrice: 20000000000,
+      gasPrice: 2000000000
     },
     ganache: {
       host: 'localhost',
@@ -29,7 +46,7 @@ module.exports = {
       provider: providerRopsten,
       network_id: 3, // eslint-disable-line camelcase
       gas: 4000000,
-      gasPrice: 20000000000,
+      gasPrice: 2000000000,
     },
   },
 };
